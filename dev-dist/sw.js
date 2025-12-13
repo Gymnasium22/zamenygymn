1fc4bbce3404ea4e5ab66c0e793ee218bbdccf78
GymnasiumAdmin <stasevichnikita050@gmail.com>
2025-12-13 16:51:49 +0300

    Исправление pwa manifest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "cdc3a2ab1e9a18cc01b5be9e541e5b38"
   }, {
     "url": "index.html",
-    "revision": "0.jjhjvlevlho"
+    "revision": "0.njcc0rih1lo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
